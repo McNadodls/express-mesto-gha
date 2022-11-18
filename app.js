9d2efa754b79023255ee01eafe288dfc5722fbd3
McNadodls <103933912+McNadodls@users.noreply.github.com>
2022-11-18 19:19:13 +0300

    V1

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 // Слушаем 3000 порт
+const app = express();
 const { PORT = 3000 } = process.env;
 
-const app = express();
-
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use((req, res, next) => {
     req.user = {
