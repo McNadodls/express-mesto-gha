b0f95a3f2e55bfaef29e1748b0c24f1114629532
McNadodls <103933912+McNadodls@users.noreply.github.com>
2022-12-21 23:51:50 +0300

    Add files via upload

--- controllers/card.js
+++ controllers/card.js
@@ -5,14 +5,14 @@ const Forbidden = require('../errors/Forbidden');
 
 module.exports.getCard = (req, res, next) => {
   Card.find({})
-    .then((card) => res.send({ data: card }))
+    .then((card) => res.send(card))
     .catch(next);
 };
 
 module.exports.createCard = (req, res, next) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
-    .then((card) => res.send({ data: card }))
+    .then((card) => res.send(card))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         next(new BadRequest('Неправильные данные'));
