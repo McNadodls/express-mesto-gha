0a459e42e20ca8a3636486f9bf241adc84651482
McNadodls <103933912+McNadodls@users.noreply.github.com>
2022-12-24 14:09:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -33,6 +33,11 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(cookieParser());
 app.use(requestLogger);
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+}); 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
